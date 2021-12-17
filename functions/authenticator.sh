917dcedf59ae44b38cea4f1fec7322067c04a7f4
BjarneDM <bjarne@mathiesen.info>
2021-12-17 12:12:40 +0100

    On branch main
    Changes to be committed:
    	new file:   bin/xpath
    		xpath function/program from macOS
    
    	modified:   functions/authenticator.sh
    	modified:   functions/cleanup.sh
    	modified:   functions/login.incl
    	modified:   new
    	modified:   renew
    		fixed paths
    		it turns out, that all paths have to be relative to the original script

--- functions/authenticator.sh
+++ functions/authenticator.sh
@@ -1,4 +1,4 @@
-#!/macports/bin/bash
+#!/opt/local/bin/bash
 
 #
 #           --manual-auth-hook /path/to/dns/authenticator.sh \
@@ -10,35 +10,35 @@
 #echo "CERTBOT_DOMAIN      : ${CERTBOT_DOMAIN}"     1>&2
 #echo "CERTBOT_VALIDATION  : ${CERTBOT_VALIDATION}" 1>&2
 
-declare -i dnssleep=300
+declare -i dnssleep=150
 
-. ./login.incl
+. ./functions/login.incl
 
 #
 #   opret TXT record
 curl \
     --silent \
-    -c ../download/cookies.txt \
-    -b ../download/cookies.txt \
+    -c ./download/cookies.txt \
+    -b ./download/cookies.txt \
     -F user_domain=${DOMAIN} \
     -F name=_acme-challenge.${CERTBOT_DOMAIN} \
     -F txtdata=${CERTBOT_VALIDATION} \
     -F ttl=300 \
     -F action=dns_primary_record_added_txt \
     -L \
-    --output ../download/gratisdns.html \
+    --output ./download/gratisdns.html \
     --url 'https://admin.gratisdns.com/'
 
 #
 #   tjek at det er gået godt
-tidy -q -asxml --numeric-entities yes ../download/gratisdns.html 2>/dev/null > ../download/gratisdns.xml
+tidy -q -asxml --numeric-entities yes ./download/gratisdns.html 2>/dev/null > ./download/gratisdns.xml
 domCount=$( \
-    xpath ../download/gratisdns.xml '//td[@class="table-success"]' 2>&1 \
+    ./bin/xpath ./download/gratisdns.xml '//td[@class="table-success"]' 2>&1 \
     | sed -E -e '1,2d' \
     | wc -l )
 if [[ ! ${domCount} -eq 1 ]]
 then
-    fejlTekst=$( xpath ../download/gratisdns.xml '//td[@class="table-danger"]/text()' 2>/dev/null )
+    fejlTekst=$( ./bin/xpath ./download/gratisdns.xml '//td[@class="table-danger"]/text()' 2>/dev/null )
     echo "fejl i oprettelsen af TXT record : ${fejlTekst}" 1>&2
     exit 1
 fi
@@ -55,7 +55,7 @@ while read -u 9 keyValue
 do
     echo "${keyValue}"
 done 9< <( \
-xpath ../download/gratisdns.xml \
+./bin/xpath ./download/gratisdns.xml \
     "//text()[.=\"${CERTBOT_VALIDATION}\"]/../..//a[contains(@href,\"dns_primary_delete_txt\")]/@href" \
     2>&1 \
 | sed -E -e '1,2d' \
--- functions/cleanup.sh
+++ functions/cleanup.sh
@@ -1,4 +1,4 @@
-#!/macports/bin/bash
+#!/opt/local/bin/bash
 
 #
 #           --manual-cleanup-hook /path/to/dns/cleanup.sh \
@@ -19,25 +19,25 @@
 #   slet TXT record
 curl \
     --silent \
-    -c ../download/cookies.txt \
-    -b ../download/cookies.txt \
+    -c ./download/cookies.txt \
+    -b ./download/cookies.txt \
     -F user_domain=${DOMAIN} \
     -F id=${CERTBOT_AUTH_OUTPUT} \
     -F action=dns_primary_delete_txt \
     -L \
-    --output ../download/gratisdns.html \
+    --output ./download/gratisdns.html \
     --url 'https://admin.gratisdns.com/'
 
 #
 #   tjek at det er gået godt
-tidy -q -asxml --numeric-entities yes ../download/gratisdns.html 2>/dev/null > ../download/gratisdns.xml
+tidy -q -asxml --numeric-entities yes ./download/gratisdns.html 2>/dev/null > ./download/gratisdns.xml
 domCount=$( \
-    xpath ../download/gratisdns.xml '//td[@class="table-success"]' 2>&1 \
+    ./bin/xpath ./download/gratisdns.xml '//td[@class="table-success"]' 2>&1 \
     | sed -E -e '1,2d' \
     | wc -l )
 if [[ ! ${domCount} -eq 1 ]]
 then
-    fejlTekst=$( xpath ../download/gratisdns.xml '//td[@class="table-danger"]/text()' 2>/dev/null )
+    fejlTekst=$( ./bin/xpath ./download/gratisdns.xml '//td[@class="table-danger"]/text()' 2>/dev/null )
     echo "fejl i sletningen af TXT record : ${fejlTekst}" 1>&2
     exit 1
 fi
